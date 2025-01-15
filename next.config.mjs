6272a736f7271e0b3468f47d78e87ac622cd00f5
doutyacodes <doutyacode@gmail.com>
2025-01-15 12:55:57 +0530

    functions

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,30 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
+// Create Next.js plugin for internationalization
 const withNextIntl = createNextIntlPlugin("./app/i18n/request.js");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: false,
   images: {
     remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.pexels.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'wowfy.in',  // Added this line to allow images from wowfy.in
-          pathname: '**',
-        },
-      ],
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wowfy.in',  // Allow images from wowfy.in
+        pathname: '**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // Exclude the ssh2 module from being bundled on the client side
+    if (!isServer) {
+      config.externals = [...(config.externals || []), 'ssh2'];
+    }
+    return config;
   },
 };
 
