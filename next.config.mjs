4a0d54e1833965c11acaf2b6e14f870656c85337
doutyacodes <doutyacode@gmail.com>
2025-01-15 13:08:40 +0530

    functions fx

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,38 @@
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// // Create Next.js plugin for internationalization
+// const withNextIntl = createNextIntlPlugin("./app/i18n/request.js");
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.pexels.com',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'wowfy.in',  // Allow images from wowfy.in
+//         pathname: '**',
+//       },
+//     ],
+//   },
+//   webpack: (config, { isServer }) => {
+//     // Exclude the ssh2 module from being bundled on the client side
+//     if (!isServer) {
+//       config.externals = [...(config.externals || []), 'ssh2'];
+//     }
+//     return config;
+//   },
+// };
+
+// export default withNextIntl(nextConfig);
+
+
 import createNextIntlPlugin from "next-intl/plugin";
 
-// Create Next.js plugin for internationalization
 const withNextIntl = createNextIntlPlugin("./app/i18n/request.js");
 
 /** @type {import('next').NextConfig} */
@@ -14,16 +46,35 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'wowfy.in',  // Allow images from wowfy.in
+        hostname: 'wowfy.in',
         pathname: '**',
       },
     ],
   },
   webpack: (config, { isServer }) => {
-    // Exclude the ssh2 module from being bundled on the client side
     if (!isServer) {
-      config.externals = [...(config.externals || []), 'ssh2'];
+      // Handle SSH2 and related modules on client side
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'ssh2': false,
+        'ssh2-sftp-client': false,
+        'crypto': false,
+        'stream': false,
+        'net': false,
+        'tls': false,
+        'fs': false
+      };
     }
+
+    // Add a specific rule for the sshcrypto.node file
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader',
+      // Skip processing .node files in client-side builds
+      loader: 'null-loader',
+      resourceQuery: /client/,
+    });
+
     return config;
   },
 };
