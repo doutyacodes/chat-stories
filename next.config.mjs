8e172c621831125228fc6d899b9072d5131432f6
doutyacodes <doutyacode@gmail.com>
2025-01-29 16:46:33 +0530

    vercel limits jsdon file

--- next.config.mjs
+++ next.config.mjs
@@ -37,57 +37,11 @@ const nextConfig = {
 
     return config;
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb', // Increase body size limit for Server Actions
+    },
+  },
 };
 
 export default withNextIntl(nextConfig);
-
-
-// import createNextIntlPlugin from "next-intl/plugin";
-
-// const withNextIntl = createNextIntlPlugin("./app/i18n/request.js");
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.pexels.com',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'wowfy.in',
-//         pathname: '**',
-//       },
-//     ],
-//   },
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       // Handle SSH2 and related modules on client side
-//       config.resolve.fallback = {
-//         ...config.resolve.fallback,
-//         'ssh2': false,
-//         'ssh2-sftp-client': false,
-//         'crypto': false,
-//         'stream': false,
-//         'net': false,
-//         'tls': false,
-//         'fs': false
-//       };
-//     }
-
-//     // Add a specific rule for the sshcrypto.node file
-//     config.module.rules.push({
-//       test: /\.node$/,
-//       use: 'node-loader',
-//       // Skip processing .node files in client-side builds
-//       loader: 'null-loader',
-//       resourceQuery: /client/,
-//     });
-
-//     return config;
-//   },
-// };
-
-// export default withNextIntl(nextConfig);
