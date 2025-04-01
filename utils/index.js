f5bda4abf39ca113cfc426cd7ab07b0d0be6358a
doutyacodes <doutyacode@gmail.com>
2025-04-01 11:00:39 +0530

    db connect file

--- utils/index.js
+++ utils/index.js
@@ -11,9 +11,9 @@ import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
   host: "68.178.163.247",
-  user: "devusr_wowfyuser",
-  database: "devusr_chat_stories",
-  password:'###Wowfy123',
+  user: "devuser_chat_stories_user",
+  database: "devuser_chat_stories",
+  password:'Wowfy#user',
   port:'3306',
 });
 
